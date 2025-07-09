58e12f32f991b0e7b253cbbf13d5d0fa15947e14
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-07-09 10:25:22 +0930

    Use DATABASE_URL instead of seperare env vars

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+// module.exports = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
+
+module.exports = pool;
